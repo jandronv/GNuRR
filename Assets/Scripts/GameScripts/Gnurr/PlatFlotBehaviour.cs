6042d4b2a0ed925fd1c8d1525b38a55191ec09f6
milvagaming <milvagaming@gmail.com>
2017-05-18 15:05:45 +0200

    trigger plataformas flotantes apañado

---
+++ Assets/Scripts/GameScripts/Gnurr/PlatFlotBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatFlotBehaviour : MonoBehaviour {
+
+    public GameObject _player;
+
+    void OnTriggerEnter(Collider _player)
+    {
+        _player.transform.Translate(0, 0, 0.4f);
+    }
+
+    private void OnTriggerExit(Collider _player)
+    {
+        _player.transform.Translate(0, 0, -0.4f);
+    }
+
+}
