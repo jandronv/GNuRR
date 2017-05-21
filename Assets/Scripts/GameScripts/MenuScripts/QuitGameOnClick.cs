d8a1129e7b59d3815bda82f63534a74a6e04cdaa
milvagaming <milvagaming@gmail.com>
2017-05-21 21:22:41 +0200

    pruebas para menús y su estructura

---
+++ Assets/Scripts/GameScripts/MenuScripts/QuitGameOnClick.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGameOnClick : MonoBehaviour {
+
+	public void OnStartPressed() {
+        Application.Quit();
+	}
+}
