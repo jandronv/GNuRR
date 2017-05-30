dd7ea3c0461ad8f03e3fa56733811bbcbc76ef66
jandronv <jandronv@gmail.com>
2017-05-30 22:24:47 +0200

    escript cambio de niveles

---
+++ Assets/Scripts/GameScripts/Gnurr/ScenesInGame/CambioDeNivelVolver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioDeNivelVolver : MonoBehaviour {
+
+
+	public string _name = "";
+
+	public void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Vida Actual: ");
+		if (other.tag == "Player")
+		{
+			PlayerMngr p = GameMgr.GetInstance().GetCustomMgrs().GetPlayerMgr();
+
+			p.Vida = other.GetComponent<Player>().Vida;
+
+
+			if (!p.CambioEscena)
+			{
+				p.CambioEscena = true;
+			}
+			GameMgr.GetInstance().GetServer<SceneMgr>().ChangeScene(_name);
+		}
+	}
+}
