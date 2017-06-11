e45f25b0dd757906455331efaaa6457d1e481b8a
milvagaming <milvagaming@gmail.com>
2017-06-11 13:29:22 +0200

    sistema de partículas del bullet

--- Assets/Scripts/GameScripts/Gnurr/Player/Bullet.cs
+++ Assets/Scripts/GameScripts/Gnurr/Player/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 
     public int TamBullet;
+    public ParticleSystem PSbullet;
 
 
     public void SetTamBullet(int TamBullet) {
@@ -25,6 +26,9 @@ public class Bullet : MonoBehaviour {
             {
                 other.GetComponent<EnemyBat>().RestaVida(TamBullet);
             }
+            PSbullet.Play();
+            //aqui debería haber algo plan que tarde 0.2 s o algo así
+            //en destruirse, pq no da tiempo a verse la animacion
 			Destroy(gameObject);
 		}
 		
