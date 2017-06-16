fc6346758942e8e5d7c0b1ed6b82e078096c8894
milvagaming <milvagaming@gmail.com>
2017-06-16 20:02:21 +0200

    Gilbert en lvl 1C Holograma de Jangah en lvl 1A Cartel de exit provisional en peluseria y selector

---
+++ Assets/Scripts/GameScripts/VacuumCleaner/pelusero.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pelusero : MonoBehaviour {
+
+    public GameObject y_button;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            y_button.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            y_button.SetActive(false);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        y_button.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
