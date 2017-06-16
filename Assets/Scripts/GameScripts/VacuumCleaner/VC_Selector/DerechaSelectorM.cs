a9e5179ada4445133c2e8b966ebb1b893b90f700
milvagaming <milvagaming@gmail.com>
2017-06-16 18:50:59 +0200

    selector de niveles modificado para que sea más cómodo de usar coins más o menos funcionales

--- Assets/Scripts/GameScripts/VacuumCleaner/VC_Selector/DerechaSelectorM.cs
+++ Assets/Scripts/GameScripts/VacuumCleaner/VC_Selector/DerechaSelectorM.cs
@@ -9,30 +9,42 @@ public class DerechaSelectorM : MonoBehaviour
     public Image fondo;
     public Sprite ima1, ima2, ima3;
     public Image mundos;
+    private Quaternion mundoseleccionado;
 
     // Use this for initialization
     void Start()
     {
-
+        mundoseleccionado = mundos.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        mundoseleccionado = mundos.transform.rotation;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //_animations.SetBool("TriggerIzq", true);
-        //Debug.LogWarning("Entra");
-        fondo.sprite = ima1;
-        mundos.transform.Rotate(0, 0, -180);
+
+        if (other.gameObject.tag == "Player")
+        {
+            fondo.sprite = ima1;
+            if (mundoseleccionado.z == 1)
+            {
+                mundos.transform.Rotate(0, 0, -180);
+            }
+
+        }
+
 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        fondo.sprite = ima2;
+        if (other.gameObject.tag == "Player")
+        {
+            fondo.sprite = ima2;
+        }
+
     }
 }
